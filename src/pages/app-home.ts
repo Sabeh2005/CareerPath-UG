375fbf5c943ed5530bc8f360feecdd0186352925
Sabeh <sabehahmed2005@gmail.com>
2026-06-13 13:47:46 +0300

    Reduce hero icon top/bottom gaps to 5px

--- src/pages/app-home.ts
+++ src/pages/app-home.ts
@@ -81,13 +81,13 @@ export class AppHome extends LitElement {
 
       .hero {
         text-align: center;
-        padding: 10px 16px 24px;
+        padding: 5px 16px 24px;
       }
 
       .hero-icon {
         width: 300px;
         height: 300px;
-        margin: 0 auto 10px;
+        margin: 0 auto 5px;
         object-fit: contain;
       }
 
